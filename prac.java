84892241c82f4262a1b0c74fb4422158815cca67
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-05-04 15:47:26 +0530

    Initial commit

---
+++ prac.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class prac{
+    public static void main(String[] args) {
+        int[] intArray;
+        intArray = new int[10];
+        intArray[0] = 1;
+        intArray[1] = 2;
+        intArray[2] = 3;
+        System.out.println(intArray[2]);
+    }
+}
+
