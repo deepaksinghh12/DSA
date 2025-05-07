5bff2c02befa87f9e8f628942bbfc4b6e7bb4827
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-05-07 22:32:04 +0530

    1.3.1

--- Section-6_ArrayList/arraylist.java
+++ Section-6_ArrayList/arraylist.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 class arraylist{
     public static void main(String[] args){
         ArrayList<Integer> numbers = new ArrayList<Integer>(30);
@@ -7,5 +8,31 @@ class arraylist{
         numbers.add(15);
         numbers.add(1,6);
         System.out.println(numbers.get(1));
+        System.out.println(numbers.size());
+
+        for (int i=0;i<numbers.size();i++){
+            System.out.println(numbers.get(i));
+        }
+        // Time complexity:O(N)
+        // Space complexity:O(1)
+
+        for (Integer letter:numbers){
+            System.out.println(letter);
+        }
+        // Time complexity:O(N)
+        // Space complexity:O(1)
+
+        Iterator<Integer> it = numbers.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+        // Time complexity:O(N) .
+        // Space complexity:O(1)
+
+        for( Integer letter:numbers){
+            if (letter == 1){
+                System.out.println(letter);
+            }
+        }
     }
 }
