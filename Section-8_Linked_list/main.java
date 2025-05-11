88070a4a5baed3fc7edd2768474f2341934af503
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-05-11 21:33:29 +0530

    1.3.4

--- Section-8_Linked_list/main.java
+++ Section-8_Linked_list/main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the linked list: ");
     }
 }
