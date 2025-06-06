23a41f0643c099fe4ebe30481227968b0f0cb1df
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-06-06 09:00:14 +0530

    patterns_01

---
+++ pattern.java
@@ -0,0 +1,24 @@
+public class pattern {
+    public static void main(String[] args) {
+        pattern3(5); 
+        pattern4(5);
+    }
+
+    static void pattern3(int n){
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= row; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }   
+    }
+
+    static void pattern4(int n){
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= row; col++){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
