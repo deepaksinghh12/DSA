7a2ba75c71bba12f637b037416178335e5fcfcc9
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-06-08 20:26:35 +0530

    update 01

--- pattern.java
+++ pattern.java
@@ -2,6 +2,7 @@ public class pattern {
     public static void main(String[] args) {
         pattern3(5); 
         pattern4(5);
+        pattern5(5);
     }
 
     static void pattern3(int n){
@@ -16,6 +17,14 @@ public class pattern {
     static void pattern4(int n){
         for(int row = 1; row <= n; row++){
             for(int col = 1; col <= row; col++){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+    static void pattern5(int n){
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=row;col++){
                 System.out.print(col);
             }
             System.out.println();
