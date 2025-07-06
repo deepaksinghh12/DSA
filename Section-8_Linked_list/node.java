a9bc8f83a9bc10130c7dcbe03263bc1cca2fd073
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-07-06 16:20:50 +0530

    Time for Linked lists

---
+++ Section-8_Linked_list/node.java
@@ -0,0 +1,9 @@
+public class node {
+    int data;
+    Node next;
+    // Constructor to initialize the node with data
+    public Node(int data){
+        this.data= data;
+        this.next=null;
+    }    
+}
