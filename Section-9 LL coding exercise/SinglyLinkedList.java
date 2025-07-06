b16967080ab43e9cdd5b27a29091771e2c165515
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-07-06 17:09:20 +0530

    Time for Linked lists

---
+++ Section-9 LL coding exercise/SinglyLinkedList.java
@@ -0,0 +1,36 @@
+public class SinglyLinkedList {
+  public Node head;
+  public Node tail;
+  public int size;
+ 
+  public Node insertSinglyLinkedList(int nodeValue) {
+    head = new Node();
+    Node node = new Node();
+    node.next = null;
+    node.value = nodeValue;
+    head = node;
+    tail = node;
+    size = 1;
+    return head;
+  }
+ 
+  // Insert Method SinglyLinkedList
+  public void push(int nodeValue) {
+    if (head == null) {
+      insertSinglyLinkedList(nodeValue);
+      return;
+    } else {
+      Node node = new Node();
+      node.value = nodeValue;
+      node.next = null;
+      tail.next = node;
+      tail = node;
+      size++;
+    }
+ 
+    
+  }
+ 
+  
+ 
+}
