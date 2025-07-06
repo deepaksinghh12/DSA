62602a70fdd3cdc55b3d10678f0c0251344fbb9a
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-07-06 16:27:38 +0530

    Time for Linked lists

--- Section-8_Linked_list/LinkedList.java
+++ Section-8_Linked_list/LinkedList.java
@@ -1,3 +1,33 @@
 public class LinkedList {
-    
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+    public void insert(int data){
+        Node newNode = new Node(data);
+
+        if(head==null){
+            head = newNode;
+        }else{
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        System.out.println("Inserted: " + data);
+    }
+    public void display() {
+        Node current = head;
+        if (current == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
 }
--- Section-8_Linked_list/main.java
+++ Section-8_Linked_list/main.java
@@ -1,3 +1,10 @@
 public class main {
-    
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList();
+
+        myLinkedList.display(); // Display empty list
+
+        myLinkedList.insert(10); // Insert first element        
+        myLinkedList.insert(20); // Insert second element
+    }
 }
