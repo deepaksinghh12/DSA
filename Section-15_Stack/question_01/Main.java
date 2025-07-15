7235487422b4d6f3b02456d2808d90d9b7547578
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-07-15 18:43:40 +0530

    Section 15 beginss

--- Section-15_Stack/question_01/Main.java
+++ Section-15_Stack/question_01/Main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class Main {
     public static void main(String[] args){
         Stack newStack = new Stack(4);
--- Section-15_Stack/question_01/Stack.java
+++ Section-15_Stack/question_01/Stack.java
@@ -7,4 +7,12 @@ public class Stack{
         this.topOfStack = -1; // Stack is initially empty
         System.out.println("The stack is created with size of:" + size);
     }
+    //isEmpty method to check if the stack is empty
+    public boolean isEmpty() {
+        if (topOfStack == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
